4e86e8be69611cd4eea94ece75a23c7267ad6dd3
zfeig <zfeig@126.com>
2018-02-06 16:34:38 +0800

    update server

--- server/app/middleware/counter.js
+++ server/app/middleware/counter.js
@@ -1,6 +1,7 @@
 module.exports = (options,app) => {
 	return async function counterMiddleware(ctx,next) {
        const startTime = Date.now();
+       ctx.set("Access-Control-Allow-Origin","*");
        await next();
        const endTime = Date.now() - startTime;
        console.log(`exec time:${endTime} ms`);
--- server/config/config.default.js
+++ server/config/config.default.js
@@ -1,13 +1,16 @@
 exports.keys = "zfeig2018";
 exports.security = {
-	'csrf': false
+	'csrf': false,
+  domainWhiteList:['http://127.0.0.1:8080','http://127.0.0.1:8000','http://127.0.0.1:7001']
 }
+exports.cors = {
+  package: 'egg-cors'
+};
 exports.view = {
 	 defaultViewEngine: 'nunjucks',
 	 mapping: {
 	    '.tpl': 'nunjucks',
 	  },
-
 }
 exports.mysql = {
 	client: {
